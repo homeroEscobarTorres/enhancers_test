891e0786bd63ab6abb3e1efe29b5cd4d8fc142a1
Homero Escobar Torres <81558998+homeroEscobarTorres@users.noreply.github.com>
2022-06-11 13:24:01 +0200

    Added keys.js for external services authentication

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+// kyes.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
